09bbf1b1fcbc93e335fd22cbe0841367720de32f
Mircea <frandesmyrce@gmail.com>
2020-05-07 15:14:40 +0300

    add HOC to person comp and passing props correctly

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-
+import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
 
@@ -22,4 +22,4 @@ class Person extends Component
     }
 };
 
-export default Person;
+export default withClass(Person, classes.Person);
